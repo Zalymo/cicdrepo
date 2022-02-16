393e53c934709da9f2c4400619f4c54a45271931
Cameron Watts <cwatts11@mail.csuchico.edu>
2022-02-16 11:50:28 -0800

    add program

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// This program is a simple intro to C++ example of debugging
+int main()
+{
+  int age;
+  string name;
+  char last_initial;
+
+  cout<<"Hi. What is your first name? ";
+  cin>>name;
+  cout<<"name, what is the first letter of your last name? ";
+  cin>>last_initial;
+  cout<<"Thanks, "<<name<<last_initial<<endl;
+  cout<<"Please also tell me how old you are: ";
+  cin>>age;
+
+  if( age < 12 )
+  {
+    cout<<"Hey kid, how do you like school?\n";
+  }
+  if( age < 18 ) {
+    cout<<"Cool!"<<endl;
+    cout<<"How's highschool going?\n";
+  } else if( age == 18 )
+  {
+    cout<<"Hey!"<<endl;
+  }
+  else
+  {
+    cout<<"Pleased to meet you!\n";
+  }
+
+  return 0;
+}
